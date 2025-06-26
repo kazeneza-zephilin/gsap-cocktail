29fd69419adc85b5901a81dee375104ecadf7cfc
kazeneza zephilin <caz@kazenezas-MacBook-Pro.local>
2025-06-27 01:08:00 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import gsap from "gsap"
+import { ScrollTrigger, SplitText } from "gsap/all"
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl flex-center text-indigo-300">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
