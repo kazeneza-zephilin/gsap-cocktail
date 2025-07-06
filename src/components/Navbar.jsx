bd6c8b20b66be3e50b3210dbcde9c52694157e98
kazeneza zephilin <caz@kazenezas-MacBook-Pro.local>
2025-07-06 14:32:59 +0200

    adding blur effect on scroll to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,41 @@
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 import { useGSAP } from "@gsap/react";
-import { navLinks } from "../../constants";
+
+import { navLinks } from "../../constants/index.js";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Navbar = () => {
     useGSAP(() => {
         const navTween = gsap.timeline({
             scrollTrigger: {
-                trigger: "nav",
-                start: "bottom top", //nav position, view port position
+                trigger: document.body,
+                start: "top -80px",
+                end: "bottom bottom",
             },
         });
-        navTween.fromTo('nav', {
-          backgroundColor: 'transparent'
-        }, {
-          backgroundColor: '#00000050', 
-          backdropFilter: 'blur(10px)',
-          duration: 1,
-          ease: 'power1.inOut'
-        })
+
+        navTween.fromTo(
+            "nav",
+            { backgroundColor: "transparent" },
+            {
+                backgroundColor: "rgba(0, 0, 0, 0.3)",
+                backdropFilter: "blur(10px)",
+                duration: 0.3,
+                ease: "power1.inOut",
+            }
+        );
     });
+
     return (
         <nav>
             <div>
-                <a className="flex items-center gap-2" href="#home">
+                <a href="#home" className="flex items-center gap-2">
                     <img src="/images/logo.png" alt="logo" />
-                    <p>Mundi Cocktail</p>
+                    <p>Velvet Pour</p>
                 </a>
+
                 <ul>
                     {navLinks.map((link) => (
                         <li key={link.id}>
@@ -37,5 +47,4 @@ const Navbar = () => {
         </nav>
     );
 };
-
 export default Navbar;
