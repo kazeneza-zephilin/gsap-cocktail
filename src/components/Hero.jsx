bc38e70076eb150855c8a2d8062d94beff0fd112
kazeneza-zephilin <kzephilin2@example.com>
2025-08-20 10:36:40 +0200

    changed: header name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -59,7 +59,7 @@ const Hero = () => {
     return (
         <>
             <section id="hero" className="noisy">
-                <h1 className="title">AFRICANO</h1>
+                <h1 className="title">Mundi</h1>
                 <img
                     src="/images/hero-left-leaf.png"
                     alt="left-leaf"
